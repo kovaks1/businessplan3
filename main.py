0f1238c69d551890e15723f81ed1f849a159479a
kovaks1 <tsvkos01@gmail.com>
2025-05-04 13:08:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+#app.mount("/static", StaticFiles(directory="static"), name="static")
 
 client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 if not os.getenv("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"):
